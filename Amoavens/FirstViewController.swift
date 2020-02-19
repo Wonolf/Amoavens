e692fbc5a733e5e7c9a70acaafa1d4d679f3be2d
Bonoso Sierra <bonoso96@outlook.es>
2020-02-19 17:41:20 +0100

    Initial Commit

---
+++ Amoavens/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Amoavens
+//
+//  Created by Bononya on 19/02/2020.
+//  Copyright © 2020 Bononya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Amoavens/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Amoavens
+//
+//  Created by Bononya on 19/02/2020.
+//  Copyright © 2020 Bononya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
